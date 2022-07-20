861091a2e6311e8ce21ccfbd8e1359f8ba2711ad
Maik Engel <maik.engel7@gmail.com>
2022-07-20 13:20:25 +0200

    Slideshow with zoom animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'maik-engel.de';
+export class AppComponent implements OnInit {
+  images = ['earth0.jpg', 'earth1.jpg', 'earth2.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000)
+  }
 }
